4b64814c61e0bbda7de2be3b60be1aef1a7527e2
Kimyoungmin <kwa124@naver.com>
2021-01-22 18:44:14 +0900

    deleted

--- 4week/src/main/java/se/hs/ac/Program.java
+++
@@ -1,9 +0,0 @@
-package se.hs.ac;
-
-import se.hs.ac.controller.CardController;
-
-public class Program {
-    public static void main(String[] args) {
-        new CardController().run();
-    }
-}
